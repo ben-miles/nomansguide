0e0208b4f19cf6a0210709b48fe03ca80c20ad1e
Ben Miles <bcgm3studios@gmail.com>
2023-03-02 10:10:19 -0500

    File Organization: Icons

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -12,7 +12,7 @@ export default {
 <template>
 	<footer>
 		<div class="copyright">© {{ publishYear }} - {{ currentYear }} <a href="https://benmiles.com/" target="_blank">Ben Miles</a>. <a href="#" target="_self">No Man's Guide</a> is not affiliated with <a href="https://www.nomanssky.com/" target="_blank">No Man's Sky</a> or <a href="https://hellogames.org/" target="_blank">Hello Games</a>. </div>
-		<div class="credits">Some data and images sourced from <a href="https://nomanssky.fandom.com/" target="_blank">No Man's Sky Wiki</a>, under a <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/" target="_blank"><img src="img/icon/cc.svg" alt="Creative Commons Logo" class="creativecommons" loading="lazy"> Creative Commons Attribution 3.0</a> license. </div>
+		<div class="credits">Some data and images sourced from <a href="https://nomanssky.fandom.com/" target="_blank">No Man's Sky Wiki</a>, under a <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/" target="_blank"><img src="../assets/images/icons/cc.svg" alt="Creative Commons Logo" class="creativecommons" loading="lazy"> Creative Commons Attribution 3.0</a> license. </div>
 	</footer>
 </template>
 
