7e364921247fb3ea24fce5c539fcaf6226226a81
Ben Miles <bcgm3studios@gmail.com>
2023-03-16 14:22:12 -0400

    Update 404 View
    Made this a little more visually interesting, but there's more that could be done!

--- src/views/404View.vue
+++ src/views/404View.vue
@@ -1,9 +1,18 @@
+<script>
+import { RouterLink } from 'vue-router'
+
+export default {}
+</script>
+
 <template>
 	<section id="404">
 		<div class="container">
 			<div class="row">
-				<div class="column">
-					<h1>This is the 404 page</h1>
+				<div class="column" style="text-align: center;">
+					<img id="gaze" src="src/assets/images/error404.png" style="max-width:360px;"/>
+					<h3>// Error 404</h3>
+					<p style="font-weight: bold;">Sorry, the page you requested could not be found.</p>
+					<p>Check the spelling of the address and try again, or <RouterLink to="/" target="_self">return to the home page</RouterLink>.</p>
 				</div>
 			</div>
 		</div>
